7ee4d542f8fc849539835e12928bdeb7c7baf048
sikonggpw <1327325804@qq.com>
2024-03-13 17:59:19 +0800

    fix: 新增登录页面

---
+++ src/pages/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <view class="login">
+    <view class="login-box">
+      <input class="uni-input" focus placeholder="自动获得焦点" />
+      <view class="login-box-title">登录</view>
+    </view>
+  </view>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
